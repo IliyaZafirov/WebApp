bb90800be290249affa17c5f66834dbc452b5874
IliyaZafirov <133087050+IliyaZafirov@users.noreply.github.com>
2023-12-02 13:55:21 +0200

    templating engine added

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const chalk = require('chalk');
-const debug = require('debug')('app');
+const debug = require('debug');
+const morgan = require('morgan');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000
+
 const app = express();
 
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('Hello from my app')
+    // res.send('response from app.js');
+    res.render('index', { title: 'Globomantics', data: ['a', 'b', 'c'] });
 })
 
-app.listen(3000, () => {
-    debug(`listening on port ${chalk.green('3000')}`);
+app.listen(PORT, () => {
+    console.log(`listening on port ${chalk.green(PORT)}`);
 })
